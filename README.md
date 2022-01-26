14c8a08c7940f3d91ee86f14f82c2b3440ebb804
SimpleZhang <simplezhang@yeahka.com>
2022-01-26 18:28:20 +0800

    feat: support customize image size

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ can be used in generate nft pics 可用于ntf头像制作
 1. add folders in img 在img文件夹中添加文件夹
 2. add images(.png) in those folder 在你添加的文件夹中添加图片
 3. run index.exe 运行index.exe
+4. type width/height for your image 输入图片宽高
 4. see pics in output 在output文件夹中查看生成的图片
 
 
@@ -17,4 +18,3 @@ can be used in generate nft pics 可用于ntf头像制作
 1. dont delete folder 'img' 不要删除img文件夹
 2. dont delete folder 'output' 不要删除output文件夹
 3. only support .PNG 只支持png格式的图片
-4. only support 64*64 只支持64\*64的图片
--- index.py
+++ index.py
@@ -1,27 +1,53 @@
 import os
 import random
 from ctypes.wintypes import RGB
+from turtle import width
+from prompt_toolkit import prompt
+from prompt_toolkit.validation import Validator, ValidationError
+from prompt_toolkit.styles import Style
 from io import BytesIO
 
 from PIL import Image 
 
 IMAGE_DIR = 'img'
 
+mystyle = Style.from_dict({
+    '':          '#00ffff',
+})
 
-def dfs (img, index):
+class NumberValidator(Validator):
+    def validate(self, document):
+        text = document.text
+
+        if text and not text.isdigit():
+            i = 0
+
+            # Get index of first non numeric character.
+            # We want to move the cursor here.
+            for i, c in enumerate(text):
+                if not c.isdigit():
+                    break
+
+            raise ValidationError(message='This input contains non-numeric characters',
+                                  cursor_position=i)
+
+
+def dfs (img, index,arr,size):
     for addImg in arr[index]:
-        tempImg = Image.new("RGBA", [64,64])
+        tempImg = Image.new("RGBA", [size['width'],size['height']])
         tempImg.paste(img,(0,0), img)
         tempImg.paste(Image.open(addImg), (0,0), Image.open(addImg))
         if index == len(arr) - 1:
+            imageName = str(random.randint(0,100000000000000)) + '.png'
+            print('output:'+ imageName)
             tempImg.save(
-                os.path.join(os.getcwd() + os.sep+'output', str(random.randint(0,100000000000000)) + '.png'),
+                os.path.join(os.getcwd() + os.sep+'output', imageName),
                 format="PNG")
         else:
-            dfs(tempImg, index+1)
+            dfs(tempImg, index+1,arr,size)
 
 
-def main():
+def main(size = {'width':64,'height':64}):
     path = os.path.join(os.getcwd(), IMAGE_DIR)
     arr = []
     for base_dir, sub_dir_list, sub_file_list in os.walk(path):
@@ -31,8 +57,10 @@ def main():
         if len(temp) > 0:
             arr.append(temp)
     for base in arr[0]:
-        dfs(Image.open(base),1)
+        dfs(Image.open(base),1,arr,size)
 
 
 if __name__ == '__main__':
-    main()
+    width = prompt('Give me ouput width(px): ',validator=NumberValidator(),style= mystyle)
+    height = prompt('Give me ouput height(px): ',validator=NumberValidator(),style= mystyle)
+    main({'width':int(width),'height':int(height)})
